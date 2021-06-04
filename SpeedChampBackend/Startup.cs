fc535452eef32021e370c2e5ea28bf5d3fab776c
wubbl0rz <wubbl0rz@gmx.de>
2021-06-04 16:02:20 +0200

    fix cors

--- SpeedChampBackend/Startup.cs
+++ SpeedChampBackend/Startup.cs
@@ -14,38 +14,53 @@ using Microsoft.OpenApi.Models;
 
 namespace SpeedChamp
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SpeedChamp", Version = "v1"});
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpeedChamp v1"));
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("allow_all",
+          builder =>
+          {
+            builder.AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed((host) => true)
+              .AllowCredentials();
+          });
+      });
+
+
+      services.AddControllers();
+      // services.AddSwaggerGen(c =>
+      // {
+      //     c.SwaggerDoc("v1", new OpenApiInfo {Title = "SpeedChamp", Version = "v1"});
+      // });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseCors("allow_all");
+      
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        // app.UseSwagger();
+        // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpeedChamp v1"));
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+  }
 }
