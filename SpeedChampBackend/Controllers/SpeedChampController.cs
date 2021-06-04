4d322d095fa882be9d3cc47848fe1f287e3f2dd0
wubbl0rz <wubbl0rz@gmx.de>
2021-06-04 17:26:07 +0200

    20mb limit

--- SpeedChampBackend/Controllers/SpeedChampController.cs
+++ SpeedChampBackend/Controllers/SpeedChampController.cs
@@ -1,35 +1,58 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ByteSizeLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace SpeedChamp.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SpeedChampController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class SpeedChampController : ControllerBase
+  {
+    public SpeedChampController()
     {
-        public SpeedChampController()
-        {
-        }
-
-        [HttpPost("/speedchamp")]
-        public async Task SpeedChamp()
-        {
-            var uploadSize = 20_000_000; //20 megabytes
-            var buffer = new byte[uploadSize];
-            
-            
-            
-            // stopuhr starten
-            // upload einlesen 
-            // wenn upload >= 20mb
-            // zeit stoppen
-            // uploadSize / zeit
-
-            await Task.Delay(1000);
-        }
     }
+
+    [HttpPost("/speedchamp")]
+    public async Task<object> SpeedChamp()
+    {
+      var uploadSize = 20_000_000; //20 megabytes
+      var buffer = new byte[uploadSize];
+      var len = 0;
+      var count = 0;
+
+      var sw = Stopwatch.StartNew();
+
+      // UPLOAD SPEED CHAMP TEST
+      while ((len = await Request.Body.ReadAsync(buffer)) > 0)
+      {
+        count += len;
+      }
+
+      sw.Stop();
+
+      if (count == uploadSize)
+      {
+        var uploadBytesPerSecond = ByteSize.FromBytes(uploadSize / sw.Elapsed.TotalSeconds);
+
+        Console.WriteLine(uploadBytesPerSecond.MebiBytes);
+        
+        return new
+        {
+          upload = uploadBytesPerSecond.MebiBytes
+        };
+      }
+
+      Console.WriteLine(count);
+
+      return new
+      {
+        upload = -1
+      };
+    }
+  }
 }
